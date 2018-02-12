47aefdfe9c0efa943820351d1115b5ee0b070ef9
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-02-12 16:49:10 +0530

    Create lar.c

---
+++ lar.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],i,temp,j,max;
+    for(i=0;i<=10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<=10;i++)
+    {
+        for(j=0;j<=10;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<=10;i++)
+    {
+        max=a[0];
+    }
+    printf("max num:%d",max);
+}
