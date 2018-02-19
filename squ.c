90893085f455453a2cb7c96fc800c086fd26d3ae
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-02-19 19:17:42 +0530

    Create squ.c

---
+++ squ.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,n;
+    printf("ënter the number");
+    scanf("%d%d",&n1,&n2);
+    n=n1*n2;
+    if(n1==n2)
+    {
+        printf("ÿes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
+    
