644171b052371097235a03437c7b7df906fa25bd
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-02-19 19:04:23 +0530

    Create new file

---
+++ mul7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("ënter the number");
+    scanf("%d",&n);
+    if(n%7==0)
+    {
+        printf("ÿes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
