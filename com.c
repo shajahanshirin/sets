75f2005aacd815394ae7c42464c4431e6fa64831
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-02-26 15:59:38 +0530

    Create com.c

---
+++ com.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+  int i,n,count=0;
+  printf("enter number");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    if(n%i==0)
+    {
+      count++;
+    }
+  }
+  if(count==2)
+  {
+    printf("no");
+  }
+  else
+  {
+    printf("yes");
+  }
+return 0;
+}
