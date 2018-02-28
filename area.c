2af232066f8eb2ca04066e8af8627a47b1e3aeb5
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-02-28 15:57:56 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float a,l,b;
+    printf("enter the number");
+    scanf("%f %f",&l,&b);
+    a=l*b;
+    printf("area of farm % .5f",a);
+    return 0;
+}
+    
+   
